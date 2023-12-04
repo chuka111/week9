4fb5c4fa71db6697fe8428bf0e88ebbc6f260e05
Chuka <g00414637@atu.ie>
2023-12-04 09:38:38 +0000

    created a new class called customer

--- src/main/java/ie/atu/person.java
+++ src/main/java/ie/atu/person.java
@@ -1,4 +1,55 @@
 package ie.atu;
 
 public class person {
+    private String name;
+    private String address;
+    private String phoneNumber;
+
+
+    public person(){
+        this.name = "";
+        this.address = "";
+        this.phoneNumber = "";
+    }
+
+    public person(String name, String address, String phoneNumber) {
+        this.name = name;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "name=" +
+                "name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                '}';
+    }
+
 }
+
