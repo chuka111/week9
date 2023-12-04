cd00e2b8d847820a7447d65fa607b04ed2f3a897
Chuka <g00414637@atu.ie>
2023-12-04 10:20:10 +0000

    created a new class to inherit person

--- src/main/java/ie/atu/customer.java
+++ src/main/java/ie/atu/customer.java
@@ -1,4 +1,38 @@
 package ie.atu;
 
-public class customer {
+public class customer extends person {
+    public customer() {
+    }
+    private String cusNum;
+    private boolean mailingList;
+
+    public customer(String name, String address, String phoneNumber, String cusNum, boolean mailingList) {
+        super(name, address, phoneNumber);
+        this.cusNum = cusNum;
+        this.mailingList = mailingList;
+    }
+
+    public String getCusNum() {
+        return cusNum;
+    }
+
+    public void setCusNum(String cusNum) {
+        this.cusNum = cusNum;
+    }
+
+    public boolean isMailingList() {
+        return mailingList;
+    }
+
+    public void setMailingList(boolean mailingList) {
+        this.mailingList = mailingList;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "customer{" +
+                "cusNum='" + cusNum + '\'' +
+                ", mailingList=" + mailingList +
+                '}';
+    }
 }
